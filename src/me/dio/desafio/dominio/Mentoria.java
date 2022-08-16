dac72e51d639fe8156df34ffc72fd4d985215204
marcoskeiji <marcoskeiji@gmail.com>
2022-08-16 04:18:35 -0300

    herança e polimorfismo

--- src/me/dio/desafio/dominio/Mentoria.java
+++ src/me/dio/desafio/dominio/Mentoria.java
@@ -1,31 +1,12 @@
 package me.dio.desafio.dominio;
 
 import java.time.LocalDate;
-import java.util.Date;
 
-public class Mentoria {
+public class Mentoria extends Conteudo {
 
-    private String titulo;
-    private String descricao;
     private LocalDate data;
 
     public Mentoria() { }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public LocalDate getData() {
         return data;
@@ -36,10 +17,15 @@ public class Mentoria {
     }
 
     @Override
+    public Double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+    @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
     }
