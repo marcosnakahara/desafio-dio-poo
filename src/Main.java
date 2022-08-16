44bb53085ebeaa526737d5568198a145fac79fcf
marcoskeiji <marcoskeiji@gmail.com>
2022-08-16 04:19:46 -0300

    regra de negócio e validação

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import me.dio.desafio.dominio.Curso;
-import me.dio.desafio.dominio.Mentoria;
+import me.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 import java.util.Date;
@@ -12,18 +11,50 @@ public class Main {
         cursoJava.setTitulo("curso Java");
         cursoJava.setDescricao("descrição curso Java");
         cursoJava.setCargaHoraria(8);
-        System.out.println(cursoJava);
+//        System.out.println(cursoJava);
 
         Curso cursoJs = new Curso();
         cursoJs.setTitulo("curso Js");
         cursoJs.setDescricao("descrição curso Js");
         cursoJs.setCargaHoraria(8);
-        System.out.println(cursoJs);
+//        System.out.println(cursoJs);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
-        System.out.println(mentoria);
+//        System.out.println(mentoria);
+
+//        Conteudo conteudo = new Curso();
+//        System.out.println(conteudo);
+//
+//        conteudo = new Mentoria();
+//        System.out.println(conteudo);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(cursoJava);
+        bootcamp.getConteudos().add(cursoJs);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev naka = new Dev();
+        naka.setNome("Naka");
+        naka.inscreverBootcamp(bootcamp);
+        System.out.println(naka);
+        naka.progredir();
+        naka.progredir();
+        System.out.println(naka);
+        System.out.println("XP: " + naka.calcularTotalXp());
+
+        Dev joao = new Dev();
+        joao.setNome("Joao");
+        joao.inscreverBootcamp(bootcamp);
+        System.out.println(joao);
+        joao.progredir();
+        joao.progredir();
+        joao.progredir();
+        System.out.println(joao);
+        System.out.println("XP: " + joao.calcularTotalXp());
     }
 }
